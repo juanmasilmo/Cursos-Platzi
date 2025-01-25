835783267f552f130f333746afc7bf6e98f093f3
= <=>
2025-01-24 23:12:50 -0300

    clase 32

--- 3-javaScript-desdeCero/31-clase-async&await/app.js
+++ 3-javaScript-desdeCero/31-clase-async&await/app.js
@@ -7,13 +7,29 @@
 }
 fetchData()//se puede llamar desde la consola del navegador, es buena idea probar cada tanto, para saber que nuestros datos se estan guardando, etc*/
 
-async function fetchData(){
+async function fetchData() {
     try {
         let response = await fetch("https://rickandmortyapi.com/api/character")
         let data = await response.json()
-        console.log(data) 
+        console.log(data)
     } catch (error) {
         console.log(error)
     }
 }
 //fetchData()//se puede llamar desde la consola del navegador, es buena idea probar
+
+const urls = ["https://rickandmortyapi.com/api/character", "https://rickandmortyapi.com/api/location","https://rickandmortyapi.com/api/episode"];
+
+async function fetchNewData() {
+    try {
+        for await(let url of urls){
+            let response = await fetch(url)
+            let data = await response.json()
+            console.log(data)
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
