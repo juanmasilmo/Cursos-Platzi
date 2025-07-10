2ded02da84a0df53768a7aaba6963aece051402d
= <=>
2025-07-09 22:49:54 -0300

    3-curso-practico-php-composer

--- 8-backend-php/3-curso-practico-php/06-composer/src/helpers.php
+++ 8-backend-php/3-curso-practico-php/06-composer/src/helpers.php
@@ -2,9 +2,10 @@
 //helpers.php
 require __DIR__ . '/vendor/autoload.php';
 
-
  // echo __DIR__ . ' hasta aca el dir ' . '/vendor/autoload.php';// la variable __DIR__ es la ruta absoluta del directorio actualdonde se encuentra el archivo index.php en este caso, si fuera en el archivo format.php seria la ruta absoluta del directorio donde se encuentra el archivo format.php que es src
-
-   use juanmasilmo\Format;
-
-   echo Format::upperCase('hola mundo');
+function upper($value) {
+    return \Juanmasilmo\Format::upperCase($value);
+    // La función upper recibe un valor y lo convierte a mayúsculas utilizando el método upperCase de la clase Format del namespace Juanmasilmo.
+    // El resultado se devuelve al lugar donde se llamó la función.
+}
+?>
